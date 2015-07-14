0c0c57193791948f06bb1e422712c0ba5b7a6cc9
Tech Camp 103 <tech103@fermium.cs.dixie.edu>
2015-07-14 12:02:00 -0600

    changed update method to put

--- Routes/api.js
+++ Routes/api.js
@@ -8,7 +8,7 @@ router.get('/api/new', EventCtrl.new);
 router.post('/api(.:format)?', EventCtrl.create);
 router.get('/api/(:_id)(.:format)?', EventCtrl.read);
 router.delete('/api/(:_id)(.:format)?', EventCtrl.destroy);
-router.update('/api/(:_id)(.:format)?', EventCtrl.update);
+router.put('/api/(:_id)(.:format)?', EventCtrl.update);
 router.get('/api/:_id/change', EventCtrl.change);
 
 module.exports = router;
